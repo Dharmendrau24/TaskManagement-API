020702042c16abb59d0d6e2d28a080092a89da0b
Dharmendra Upadhyay <dharmendraupadhyay24@gmail.com>
2026-09-18 22:02:10 +0530

    chore: use Alembic for database migrations

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from src.utils.db import Base, engine
+
 from src.tasks.router import task_routes
 from src.user.router import user_routes
 
 
-Base.metadata.create_all(engine)
+app = FastAPI(
+    title="Task Management API",
+    version="1.0.0",
+)
 
-app = FastAPI(title="Task Management API", version="1.0.0")
 app.include_router(task_routes)
 app.include_router(user_routes)
